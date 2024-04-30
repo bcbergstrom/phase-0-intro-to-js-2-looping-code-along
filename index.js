2871ecdebbc8033c04004a5ebf58643b1739a6cb
Benton Bergstrom <bbergstrom0524@gmail.com>
2024-04-30 14:05:53 -0600

    Final Push

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function countDown(posInt){
+    for(let i = posInt; i > 0 ; i--){
+        console.log(i);
+    }
+    console.log(0);
+}
+
+function writeCards(strArray, eventName){
+    let temp = [];
+    for(let i = 0; i < strArray.length; i++){
+        let str = (`Thank you, ${strArray[i]}, for the wonderful ${eventName} gift!`);
+        temp = [...temp, str];
+    }
+    return temp;
+}
